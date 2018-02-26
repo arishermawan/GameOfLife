7bdb24d35f7dad0240149e28351666b98dbcf9bf
rizhima <arishermawan@hotmail.com>
2018-02-26 10:17:34 +0530

    create universe class

---
+++ src/main/java/com/gojek/bootcamp/drillv3/Universe.java
@@ -0,0 +1,4 @@
+package com.gojek.bootcamp.drillv3;
+
+public class Universe {
+}
---
+++ src/test/java/com/gojek/bootcamp/drillv3/UniverseTest.java
@@ -0,0 +1,8 @@
+package com.gojek.bootcamp.drillv3;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class UniverseTest {
+}
