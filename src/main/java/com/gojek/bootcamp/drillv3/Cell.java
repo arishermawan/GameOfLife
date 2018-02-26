1fc679491386760da768533ad2d0aefd1df4905b
rizhima <arishermawan@hotmail.com>
2018-02-26 14:24:46 +0530

    cell should return status

---
+++ src/main/java/com/gojek/bootcamp/drillv3/Cell.java
@@ -0,0 +1,17 @@
+package com.gojek.bootcamp.drillv3;
+/*
+ Represent cell dead or alive in universe
+ */
+
+public class Cell {
+    int status;
+
+    public Cell() {
+        this.status = 0;
+    }
+
+    public boolean isAlive() {
+        return false;
+    }
+
+}
---
+++ src/test/java/com/gojek/bootcamp/drillv3/CellTest.java
@@ -0,0 +1,16 @@
+package com.gojek.bootcamp.drillv3;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class CellTest {
+    @Test
+    public void cellShouldReturnStatus(){
+        Cell cell = new Cell();
+        assertThat(cell.isAlive(), is(equalTo(false)));
+    }
+
+}
